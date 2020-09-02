4b1f5e6e59e0b701a6f224aba4bb0f631d67468a
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-02 17:05:47 +0530

    Commit#34

---
+++ master_yoda.py
@@ -0,0 +1,6 @@
+def master_yoda(text):
+    target_list = text.split()
+    #print(' '.join(target_list[::-1]))
+    return ' '.join(target_list[::-1])
+master_yoda('All you had to do was to follow the train CJ')
+
