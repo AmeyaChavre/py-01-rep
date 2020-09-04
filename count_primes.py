a2e35d0da32ac3b920ac4f84d45cbed6f6979ff8
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-04 15:42:32 +0530

    Commit#42

--- count_primes.py
+++ count_primes.py
@@ -1,9 +1,12 @@
 def count_primes(num):
     list_of_primes=[]
+    target_list=[]
     for i in range(2,num+1):
         if i not in list_of_primes:
-            print(i)
+            target_list.append(i)
             for j in range(i*i,num+1,i):
                 list_of_primes.append(j)
+    #print(target_list)
+    return len(target_list)
 count_primes(100)
         
