02b4627a8f8a976db20a5a8aa9f2c8f87e7353c5
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-05 23:46:25 +0530

    Commit#43

--- square_num_map.py
+++ square_num_map.py
@@ -4,3 +4,4 @@ def square_num(num):
 list_of_numbers = [1,2,3,4,5,6,7,8,9]
 for item in map(square_num,list_of_numbers):
     print(item)
+print("Squared List: ", list(map(square_num,list_of_numbers)))
