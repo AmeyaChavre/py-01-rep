17e5466e19c3549ab16c8aaafe3de98f53cc7d75
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-15 18:45:17 +0530

    Commit#65

---
+++ div_by_0.py
@@ -0,0 +1,12 @@
+try:
+    x = 5
+    y = 0
+
+    z = x/y
+    print(z)
+
+except:
+    print("Are we dividing by 0!?")
+    
+finally:
+    print("All done")
---
+++ h-W-g-A.py
@@ -0,0 +1,7 @@
+try:
+    for i in ['a','b','c']:
+        print(i**2)
+except:
+    print("Oops something went wrong!!")
+finally:
+    print("Here we go again")
