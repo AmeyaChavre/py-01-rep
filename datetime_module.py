30e7d64d6934aa62b36e875b5927e7c714bf882b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-21 18:38:16 +0530

    Commit#78

--- datetime_module.py
+++ datetime_module.py
@@ -8,4 +8,8 @@ today = datetime.date.today()
 print(f"{today}")
 print(f"{today.ctime()}")
 
+from datetime import datetime
 
+my_date_time = datetime(2028,10,30,15,21,2,29)
+
+print(f"{my_date_time}")
