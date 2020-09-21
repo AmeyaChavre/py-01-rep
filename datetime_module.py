d74e1aca8f98da33c39915f1326fdb92a6664b90
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-21 18:35:30 +0530

    Commit#77

--- datetime_module.py
+++ datetime_module.py
@@ -4,4 +4,8 @@ mytime = datetime.time(10,10,10,10)
 print(mytime)
 print(f"Hour:{mytime.hour} Minute:{mytime.minute} Second:{mytime.second} Microsecond:{mytime.microsecond}")
 
+today = datetime.date.today()
+print(f"{today}")
+print(f"{today.ctime()}")
+
 
