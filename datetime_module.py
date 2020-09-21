e263af56eef753fbbc6f580e3a08dd65457c6cd5
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-21 18:27:36 +0530

    Commit#76

---
+++ datetime_module.py
@@ -0,0 +1,7 @@
+import datetime
+
+mytime = datetime.time(10,10,10,10)
+print(mytime)
+print(f"Hour:{mytime.hour} Minute:{mytime.minute} Second:{mytime.second} Microsecond:{mytime.microsecond}")
+
+
