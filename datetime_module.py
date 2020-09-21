19ac2dd2736264a75cd395189a72d97e8fb7689c
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-21 18:46:56 +0530

    Commit#80

--- datetime_module.py
+++ datetime_module.py
@@ -15,3 +15,12 @@ my_date_time = datetime(2028,10,30,15,21,2,29)
 print(f"{my_date_time}")
 updated_date_time = my_date_time.replace(2050)
 print(f"{updated_date_time}")
+
+from datetime import date
+
+date1 = date(2022,12,25)
+date2 = date(2023,12,25)
+
+difference_between_date = date2 - date1
+
+print(f"The difference between {date2} and {date1} : {difference_between_date}")
