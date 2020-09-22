f578cbb1e50e6f0e9e9e955a0b5fffefb29b52dc
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-22 16:31:27 +0530

    Commit#77

--- regex_ph_no.py
+++ regex_ph_no.py
@@ -2,7 +2,9 @@ import re
 
 text = "This is a very long paragraph , with phone numbers. Whitehouse : 555-555-5555 . DisneyLand : 888-878-9090 . Police Department : 911-119-911. McDonalds : 667-687-6767. Fire Department : 999-911-9111. US Coast Guard : 123-232-7867."
 count=0
-for phone_no in re.finditer(r'\d{3}-\d{3}-\d{4}',text):
+for phone_no in re.finditer(r'(\d{3})-(\d{3})-(\d{4})',text):
     count+=1
     print(f"\tPhone Number {count}: {phone_no.group()}\n")
+    print(f"\tPhone Number First 3-digits : {phone_no.group(1)}\n")
 print(f"\tThere are {count} phone numbers in the text.")
+
