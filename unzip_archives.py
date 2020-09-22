bf4596446c74c707a962f86899b3928d09c9f208
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-22 18:56:09 +0530

    Commit#83

---
+++ unzip_archives.py
@@ -0,0 +1,7 @@
+import shutil
+
+file_path = "C:\\Users\\hp\\Desktop\\unzip_me_for_instructions.zip"
+
+shutil.unpack_archive("C:\\Users\\hp\\Desktop\\unzip_me_for_instructions.zip","C:\\Users\\hp\\Desktop\\instructions","zip")
+
+
