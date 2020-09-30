a5c80c52f945166b6e3aac05a55a3946faae33a6
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-30 20:54:51 +0530

    Commit#85

---
+++ Spreadsheets and PDFs/csv_data_extrct.py
@@ -0,0 +1,36 @@
+import csv
+
+open_file = open("C:\\Users\\User-PC\\Desktop\\example.csv",encoding="utf-8")
+
+csv_data = csv.reader(open_file)
+
+extracted_data = list(csv_data)
+
+print(f"Extracted Data : {extracted_data}\n")
+print(f"Number of rows in the spreadsheet : {len(extracted_data)-1}\n")
+print("List of Columns:\n")
+column_count=0
+for column_name in extracted_data[0]:
+    column_count+=1
+    print(f"\tColumn {column_count} : {column_name}")
+print(f"\nNumber of columns in the spreadsheet : {column_count}\n")
+mailing_list=[]
+
+for emails in extracted_data[1:]:
+    mailing_list.append(emails[3])
+print(f"List of all emails in spreadsheet : {mailing_list}")
+
+amazon_accounts = []
+for any_email in mailing_list:
+    if "amazon.com" in any_email:
+        amazon_accounts.append(any_email)
+print(f"\nList of Amazon Accounts : {amazon_accounts}")
+    
+    
+    
+
+
+
+
+
+
